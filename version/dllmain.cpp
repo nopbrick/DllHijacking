9f6443a6780b879d3596d877c34dcfb285ac441d
Filip Ceglik <fceglik@gmail.com>
2022-06-23 10:15:23 +0200

    initial upload

---
+++ version/dllmain.cpp
@@ -0,0 +1,28 @@
+#include "pch.h"
+#include <iostream>
+#include <Windows.h>
+
+void Execute()
+{
+
+}
+
+
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        Execute();
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
